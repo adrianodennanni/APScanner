c21088de9514e1deb3b813669817cf72de88961c
Adriano Dennanni <adrianodennanni@gmail.com>
2016-07-17 21:36:16 -0300

    Opening app will redirect user to right Activity

---
+++ app/src/main/java/map/net/apscanner/activities/StartActivity.java
@@ -0,0 +1,33 @@
+package map.net.apscanner.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * This is the Launcher Activity. If the user has previously logged in, it will warp to the User's
+ * Facilities Activity.
+ * If the user is not logged in, the next activity will be LoginActivity
+ */
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = this.getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        Intent nextActivity;
+
+        if (prefs.getBoolean("logged_in?", false)) {
+            nextActivity = new Intent(StartActivity.this, FacilitiesActivity.class);
+        } else {
+            nextActivity = new Intent(StartActivity.this, LoginActivity.class);
+        }
+
+        startActivity(nextActivity);
+        finish();
+    }
+}

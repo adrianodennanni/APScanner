8c94f87ba6738b5b09bef70b69b62e7a57afccbe
Adriano Dennanni <adrianodennanni@gmail.com>
2016-07-20 02:27:02 -0300

    creating AsyncTask for facilities activity

--- app/src/main/java/map/net/apscanner/activities/FacilitiesActivity.java
+++ app/src/main/java/map/net/apscanner/activities/FacilitiesActivity.java
@@ -1,9 +1,11 @@
 package map.net.apscanner.activities;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import map.net.apscanner.R;
+import okhttp3.Response;
 
 public class FacilitiesActivity extends AppCompatActivity {
 
@@ -12,4 +14,20 @@ public class FacilitiesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facilities);
     }
+
+    /**
+     * This async task gets a list of User's facilities data from server and put them into a
+     * ListView. The user can touch on the facility to access its zones.
+     */
+    private class getFacilitiesFromServer extends AsyncTask<Void, Void, Response> {
+
+        /**
+         * TODO Implement this
+         **/
+        @Override
+        protected Response doInBackground(Void... params) {
+            return null;
+        }
+    }
 }
+

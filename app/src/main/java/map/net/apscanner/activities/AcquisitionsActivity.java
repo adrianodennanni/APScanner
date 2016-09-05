928b73cfdcf1fc3aa816fa0c8f9c49a9176eb48f
Adriano Dennanni <adrianodennanni@gmail.com>
2016-09-05 15:01:47 -0300

    Using ButterKnife

--- app/src/main/java/map/net/apscanner/activities/AcquisitionsActivity.java
+++ app/src/main/java/map/net/apscanner/activities/AcquisitionsActivity.java
@@ -1,12 +1,20 @@
 package map.net.apscanner.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
 import map.net.apscanner.R;
 
 public class AcquisitionsActivity extends AppCompatActivity {
 
+    @BindView(R.id.fabStartMeasure)
+    FloatingActionButton startAcquisitionButton;
+    @BindView(R.id.imageButtonEraseCurrentSet)
+    FloatingActionButton eraseCurrentSetButton;
+    @BindView(R.id.imageButtonSendSet)
+    FloatingActionButton sendCurrentSets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

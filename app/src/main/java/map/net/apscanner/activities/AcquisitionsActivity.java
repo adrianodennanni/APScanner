8d93116ef85c872b85e11ce907e54602d47b97fb
Adriano Dennanni <adrianodennanni@gmail.com>
2016-09-07 18:55:47 -0300

    Asking for permission to access Location

--- app/src/main/java/map/net/apscanner/activities/AcquisitionsActivity.java
+++ app/src/main/java/map/net/apscanner/activities/AcquisitionsActivity.java
@@ -1,6 +1,10 @@
 package map.net.apscanner.activities;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
@@ -11,7 +15,11 @@ import butterknife.ButterKnife;
 import map.net.apscanner.R;
 import map.net.apscanner.classes.zone.Zone;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
 public class AcquisitionsActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ACCESS_LOCATION = 0;
 
     @BindView(R.id.fabStartMeasure)
     FloatingActionButton startAcquisitionButton;
@@ -42,6 +50,59 @@ public class AcquisitionsActivity extends AppCompatActivity {
             subtitleAcquisitionTextView.setText(zone.getName());
         }
 
+        mayRequestLocationAccess();
+
 
     }
+
+
+    private void startScan(String normalizationAlgorithm, int scansPerAcquisition, float interval) {
+
+    }
+
+
+    /**
+     * This function asks for permission to access Coarse Location, necessary to read access points
+     * data.
+     *
+     * @return if ACCESS_COARSE_LOCATION has been permitted (true) or not (false)
+     */
+    private boolean mayRequestLocationAccess() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission(ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION)) {
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_LOCATION);
+        } else {
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_LOCATION);
+        }
+        return false;
+    }
+
+    /**
+     * This function is called by the System after the user has chosen to permit or not
+     * Coarse Location to be accessed by the app.
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_ACCESS_LOCATION) {
+            Intent intent = new Intent(AcquisitionsActivity.this, AcquisitionsActivity.class);
+            intent.putExtra("ZONE", zone);
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(intent);
+                finish();
+            } else {
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/map/net/apscanner/classes/acquisition_set/AcquisitionSet.java
@@ -0,0 +1,7 @@
+package map.net.apscanner.classes.acquisition_set;
+
+/**
+ * Created by adriano on 9/7/16.
+ */
+public class AcquisitionSet {
+}

180a554a50f2b552c56af604133e06cf5169b334
Adriano Dennanni <adrianodennanni@gmail.com>
2016-11-06 13:18:13 -0200

    Icons for training dataset

--- app/src/main/java/map/net/apscanner/fragments/PredictZonesFragment.java
+++ app/src/main/java/map/net/apscanner/fragments/PredictZonesFragment.java
@@ -1,7 +1,23 @@
 package map.net.apscanner.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import map.net.apscanner.R;
 
 public class PredictZonesFragment extends Fragment {
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.current_acquisitions_list_fragment, container, false);
+        ButterKnife.bind(this, view);
+
+
+        return view;
+    }
 }
